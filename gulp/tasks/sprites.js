05ff3d4bf35dc9ec3665c646c27bfabb89d45318
Me <gh.2018.08.26@outlook.com>
2018-09-15 10:47:03 -0500

    gulpfile update

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -13,8 +13,7 @@ var config = {
    }
 }
 
-gulp.task('createSprite', function () {
+gulp.task('createSprite', function() {
    return gulp.src('./app/assets/images/icons/**/*.svg')
-      .pipe(svgSprite(config))
       .pipe(gulp.dest('./app/temp/sprite/'));
 });
